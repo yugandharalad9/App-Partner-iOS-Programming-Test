cca4030d7fb12ec6921a138c70caf9de252624a8
yugandharalad9 <30602646+yugandharalad9@users.noreply.github.com>
2018-01-05 11:35:19 -0500

    Login Request successful in LoginViewController.m file

--- iOSTest/View Controllers/LoginViewController.m
+++ iOSTest/View Controllers/LoginViewController.m
@@ -12,6 +12,7 @@
 
 @interface LoginViewController ()
 @property (nonatomic, strong) LoginClient *client;
+@property (nonatomic, strong) UIAlertController *alertController;
 @property (weak, nonatomic) IBOutlet UITextField *email;
 @property (weak, nonatomic) IBOutlet UITextField *password;
 
@@ -52,7 +53,7 @@
 {
     [super viewDidLoad];
     self.title = @"Login";
-    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"hehe" style:UIBarButtonItemStylePlain target:nil action:nil];
+    //self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"hehe" style:UIBarButtonItemStylePlain target:nil action:nil];
 
 }
 
@@ -62,63 +63,21 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)backAction:(id)sender
-{
-    MenuViewController *mainMenuViewController = [[MenuViewController alloc] init];
-    [self.navigationController pushViewController:mainMenuViewController animated:YES];
-}
+
 
 - (IBAction)didPressLoginButton:(id)sender
 {
     //Mark: - Log In request sucessful
-    {
-        UIAlertController * alert = [UIAlertController
-                                     alertControllerWithTitle:@"LogIn"
-                                     message:@"Log In Successful!!"
-                                     preferredStyle:UIAlertControllerStyleAlert];
-        
-        //Add Buttons
-        
-        UIAlertAction* continueButton = [UIAlertAction
-                                    actionWithTitle:@"Continue"
-                                    style:UIAlertActionStyleDefault
-                                    handler:^(UIAlertAction * action) {
-                                    }];
-        
-        UIAlertAction* cancelButton = [UIAlertAction
-                                   actionWithTitle:@"Cancel"
-                                   style:UIAlertActionStyleDefault
-                                   handler:^(UIAlertAction * action) {
-                                   }];
-        
-        //Add your buttons to alert controller
-        
-        [alert addAction:continueButton];
-        [alert addAction:cancelButton];
-        
-        [self presentViewController:alert animated:YES completion:nil];
-    }
+    
+    [self myFunc];
     
     
-    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"http://dev3.apppartner.com/AppPartnerDeveloperTest/scripts/login.php"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        NSLog(@"response %@", response);
-        NSLog(@"data %@", data);
-        
-        //NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        //NSLog(@"%@", str);
-        
-        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
-        
-        NSLog(@"%@", dict);
-        
-        
-        
-        
-        
-    }
-      ] resume
-     ];
+    
+    
+   
+    
+    
+   
     //Mark: - Log In request failed
     
     
@@ -146,33 +105,89 @@
 
 
 
-- (IBAction)loginButton:(id)sender {
+- (void)myFunc{
     
-  /*  @try {
+   /* @try {
         
         if ([[_email text] isEqualToString:@""] || [[_password text] isEqualToString:@""]) {
             [self alertFailed:@"Please enter correct username and password" :@"Login Failed!"];
             
-        } else { */
-
+        } else {
+*/
    
-    /* NSString *post = [[NSString alloc] initWithFormat:@"username=%@&password=%@", [_email text], [_password text]];
+     NSString *post = [[NSString alloc] initWithFormat:@"username=%@&password=%@", [_email text], [_password text]];
     NSLog(@"PostData: %@", post);
     NSURL *url = [NSURL URLWithString:@"http://dev3.apppartner.com/AppPartnerDeveloperTest/scripts/login.php"];
-    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-    NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    //NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    //NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     [request setURL:url];
     [request setHTTPMethod:@"Post"];
-    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    [request setValue:@"application/x-www-form-urlencoder"  forHTTPHeaderField:@"Content-Type"];
+    //[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    //[request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/x-www-form-urlencoded"  forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSLog(@"request %@", request);
+    
+    //NSURLConnection *connection= [[NSURLConnection alloc] initWithRequest:request delegate:self];
     
     //Mark: - NSURLRequest
-    NSError *error = [[NSError alloc] init];
-    NSHTTPURLResponse *response = nil;
+    //NSError *error = [[NSError alloc] init];
+    //NSHTTPURLResponse *response = nil;
     
-    NSDate *startTime = [NSDate date]; */
+    NSDate *startTime = [NSDate date];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      
+                                      NSLog(@"response %@", response);
+                                      NSLog(@"data %@", data);
+                                      
+                                      
+                                      if (data == nil) {
+                                          [self printCannotLoad];
+                                      } else {
+                                          [self parseWeatherJSON:data];
+                                      }
+                                  }];
+    [task resume];
+    
+    
+    /*[[[NSURLSession sharedSession] dataTaskWithRequest:<#(nonnull NSURLRequest *)#>:[NSURL URLWithString:@"http://dev3.apppartner.com/AppPartnerDeveloperTest/scripts/login.php"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"response %@", response);
+        NSLog(@"data %@", data);
+        
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", str);
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+        
+        NSLog(@"%@", dict);
+        
+
+        
+    }
+      ] resume
+     ];*/
+    
+    NSLog(@"Time: %f", -[startTime timeIntervalSinceNow]);
+    
+   /* NSLog(@"Response code: %d", [response statusCode]);
+    if ([response statusCode] >=200 && [response statusCode] <300) {
+        NSString *responseData = [[NSString alloc] initWithData:startTime encoding:NSUTF8StringEncoding]
+    }*/
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -181,4 +196,65 @@
     
     
 }
+
+- (void) parseWeatherJSON:(NSData *) jsonData {
+    NSLog(@"Here");
+    
+    NSString *str = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", str);
+    
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableLeaves error:nil];
+    
+    NSLog(@"%@", dict);
+    dispatch_async(dispatch_get_main_queue(),^{
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"LogIn"
+                                     message:@"Log In Successful!!"
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        //Add Buttons
+        
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:@"OK"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       
+                                       NSLog(@"Hello Mr OK");
+                                       
+                                       [self.navigationController popViewControllerAnimated:YES];
+                                       
+                                       
+                                       
+                                       
+                                       
+                                   }];
+        
+        UIAlertAction* cancelButton = [UIAlertAction
+                                       actionWithTitle:@"Cancel"
+                                       style:UIAlertActionStyleDefault
+                                       handler:^(UIAlertAction * action) {
+                                           
+                                           NSLog(@"Hello Mr Cancel");
+                                           
+                                           
+                                           
+                                       }];
+        
+        //Add your buttons to alert controller
+        
+        [alert addAction:okButton];
+        [alert addAction:cancelButton];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    });
+        
+    
+}
+
+- (void) printCannotLoad {
+    NSLog(@"Here2");
+    
+}
+    
 @end
